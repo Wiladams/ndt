c1915f512d723fb0683ec17a4bd725e4498b2158
William Adams <wiladams@microsoft.com>
2020-06-28 14:49:03 -0700

    touchy full screen

--- projects/touchy/touchy.cpp
+++ projects/touchy/touchy.cpp
@@ -6,11 +6,23 @@ using namespace p5;
 
 
 std::unordered_map<int, TouchEvent> touches;
+std::unordered_map<int, Pixel> tcolor;
+
+Pixel randomColor()
+{
+	return color(random(255), random(255), random(255));
+}
 
 void setup()
 {
-	createCanvas(1920, 1080);
+	//createCanvas(1920, 1080);
+	createCanvas(displayWidth, displayHeight);
+	layered();
+	setWindowPosition(0, 0);
 	//frameRate(60);
+	
+	// turn on using raw touch input
+	// precludes gesture processing
 	touch();
 }
 
@@ -20,7 +32,7 @@ void drawTouch(std::pair<int,TouchEvent> pe)
 	auto e = pe.second;
 
 	noStroke();
-	fill(0xc0);
+	fill(tcolor[idx]);
 	ellipse(e.x, e.y, e.w, e.h);
 }
 
@@ -33,10 +45,18 @@ void draw()
 	std::for_each(touches.begin(), touches.end(), drawTouch);
 }
 
+void keyReleased(const KeyEvent& e)
+{
+	if (e.keyCode == VK_ESCAPE)
+		halt();
+
+
+}
 void touchPressed(const TouchEvent& e)
 {
 	//printf("touchPressed: %d, %d  %dx%d\n", e.x, e.y, e.w, e.h);
 	touches.insert_or_assign(e.id, e);
+	tcolor.insert_or_assign(e.id, randomColor());
 }
 
 void touchReleased(const TouchEvent& e)
@@ -44,6 +64,7 @@ void touchReleased(const TouchEvent& e)
 	//printf("touchReleased: %d, %d\n", e.x, e.y);
 	// delete event id from map
 	touches.erase(e.id);
+	tcolor.erase(e.id);
 }
 
 void touchMoved(const TouchEvent& e)
