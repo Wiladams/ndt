a8a66ac1c50d90e306865205a87ab77e8b695963
William Adams <Wiladams@users.noreply.github.com>
2020-09-13 15:18:42 -0700

    playing with touch

--- projects/touchy/touchy.cpp
+++ projects/touchy/touchy.cpp
@@ -22,7 +22,8 @@ void setup()
 	
 	// turn on using raw touch input
 	// precludes gesture processing
-	touch();
+	auto isTouchy = touch();
+	std::cout << "isTouchy: " << isTouchy << std::endl;
 }
 
 void drawTouch(std::pair<int,TouchEvent> pe)
@@ -52,14 +53,14 @@ void keyReleased(const KeyboardEvent& e)
 
 void touchStarted(const TouchEvent& e)
 {
-	//printf("touchPressed: %d, %d  %dx%d\n", e.x, e.y, e.w, e.h);
+	printf("touchPressed: %d, %d  %dx%d\n", e.x, e.y, e.w, e.h);
 	touches.insert_or_assign(e.id, e);
 	tcolor.insert_or_assign(e.id, randomColor());
 }
 
 void touchEnded(const TouchEvent& e)
 {
-	//printf("touchReleased: %d, %d\n", e.x, e.y);
+	printf("touchReleased: %d, %d\n", e.x, e.y);
 	// delete event id from map
 	touches.erase(e.id);
 	tcolor.erase(e.id);
@@ -67,7 +68,7 @@ void touchEnded(const TouchEvent& e)
 
 void touchMoved(const TouchEvent& e)
 {
-	//printf("touchMoved: %d, %d  %dx%d\n", e.x, e.y, e.w, e.h);
+	printf("touchMoved: %d, %d  %dx%d\n", e.x, e.y, e.w, e.h);
 	touches.insert_or_assign(e.id, e);
 }
 
